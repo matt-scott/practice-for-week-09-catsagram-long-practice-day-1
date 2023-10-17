a42a26d24c3acb297c0f414f7c7286211bd42b88
Matt Scott <mattscott251@gmail.com>
2023-10-17 18:49:54 -0500

    progress on phase 2

--- public/index.js
+++ public/index.js
@@ -1,4 +1,4 @@
-import { createMainContent } from './main.js';
+import { createMainContent, getNewCat } from './main.js';
 
 // create outer div for centering all page content
 const createPageLayout = () => {
@@ -10,11 +10,66 @@ const createPageLayout = () => {
 
 
 window.onload = () => {
+/* ----------------------------------------------- */
+/* ---------- HTML Content Construction ---------- */
+/* ----------------------------------------------- */
+
     // create outer div
     const pageBody = createPageLayout();
-    
+
     // create inner content, with reference to outer div
     createMainContent(pageBody);
+
+    // new cat listener function
+    const newCatButtonListener = () => {
+        const catImageElement = document.querySelector('#cat-image');
+        getNewCat(catImageElement);
+
+        // update score count in html to 0
+        const voteScoreCount = document.querySelector('#score-count');
+        voteScoreCount.innerText = ('0');
+        // update score count in session memory to 0
+        sessionStorage.setItem("score-count", 0);
+    };
+
+/* ---------------------------------------------- */
+/* ---------- Event Listener Functions ---------- */
+/* ---------------------------------------------- */
+
+    // upvote button listener function
+    const upvoteButtonListener = () => {
+        const voteScoreCount = document.querySelector('#score-count');
+        let count = sessionStorage.getItem('score-count');
+        count++;
+        sessionStorage.setItem('score-count', count);
+        voteScoreCount.innerText = (count);
+    };
+
+    // downvote button listener function
+    const downvoteButtonListener = () => {
+        const voteScoreCount = document.querySelector('#score-count');
+        let count = sessionStorage.getItem('score-count');
+        count--;
+        sessionStorage.setItem('score-count', count);
+        voteScoreCount.innerText = (count);
+    };
+
+/* --------------------------------------------- */
+/* ---------- Event Listener Creation ---------- */
+/* --------------------------------------------- */
+
+    // create listener for new cat button click
+    const newCatButton = document.querySelector('#new-cat-button');
+    newCatButton.addEventListener("click", newCatButtonListener);
+
+    // create listener for upvote button click
+    const upvoteButton = document.querySelector('#upvote-button');
+    upvoteButton.addEventListener("click", upvoteButtonListener);
+
+    // create listener for downvote button click
+    const downvoteButton = document.querySelector('#downvote-button');
+    downvoteButton.addEventListener("click", downvoteButtonListener);
+
 };
 
 
--- public/main.js
+++ public/main.js
@@ -12,6 +12,39 @@ export const createMainContent = (pageBody) => {
     const imgDiv = document.createElement('div');
     imgDiv.className = ('cat-img-div');
 
+    // create new cat button
+    const newCatButton = document.createElement('button');
+    newCatButton.id = ('new-cat-button');
+    newCatButton.className = ('new-cat-button');
+    newCatButton.innerText = ('GET NEW CAT!');
+
+    // create vote button div
+    const voteButtonDiv = document.createElement('div');
+    voteButtonDiv.className = ('vote-button-div');
+
+    // create upvote button
+    const upvoteButton = document.createElement('button');
+    upvoteButton.id = ('upvote-button');
+    upvoteButton.className = ('button');
+    upvoteButton.innerText = ('Upvote');
+
+    // create downvote button
+    const downvoteButton = document.createElement('button');
+    downvoteButton.id = ('downvote-button');
+    downvoteButton.className = ('button');
+    downvoteButton.innerText = ('Downvote');
+
+    // create upvote/downvote score text
+    const voteScoreText = document.createElement('p');
+    voteScoreText.innerText = ('Popularity Score: ');
+    voteScoreText.className = ('score-text');
+
+    // create upvote/downvote score count
+    const voteScoreCount = document.createElement('p');
+    voteScoreCount.innerText = ('0');
+    voteScoreCount.className = ('score-text');
+    voteScoreCount.id = ('score-count');
+
     // append header
     pageBody.appendChild(header);
 
@@ -21,11 +54,30 @@ export const createMainContent = (pageBody) => {
     // append cat image
     getNewCat(catImage);
     imgDiv.appendChild(catImage);
+
+    // append new cat button
+    pageBody.appendChild(newCatButton);
+
+    // append upvote/downvote score text
+    pageBody.appendChild(voteScoreText);
+
+    // append upvote/downvote score
+    voteScoreText.appendChild(voteScoreCount);
+
+    // append vote button div
+    pageBody.appendChild(voteButtonDiv);
+
+    // append upvote button
+    voteButtonDiv.appendChild(upvoteButton);
+
+    // append downvote button
+    voteButtonDiv.appendChild(downvoteButton);
+
 };
 
 // fetch random cat from TheCatAPI
 // https://developers.thecatapi.com/view-account/ylX4blBYT9FaoVd6OhvR?report=bOoHBz-8t
-const getNewCat = (catImage) => {
+export const getNewCat = (catImage) => {
     const url = ('https://api.thecatapi.com/v1/images/search');
     try {
         fetch(url)
@@ -41,4 +93,4 @@ const getNewCat = (catImage) => {
     catch (error) {
         console.log('Failed to retrieve cat image', e);
     }
-}
+};
